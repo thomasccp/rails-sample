7e1ca123b80378bb4b6a36ab81ef5160e6306df6
thomasccp <thomasccp@msn.com>
2013-11-16 00:20:28 +0000

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(pages).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
 		end
 	end
 
@@ -23,7 +23,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(pages).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 
@@ -35,7 +35,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			expect(pages).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
 end
