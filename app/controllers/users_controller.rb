a45bbf296c2a0ed7ace38e088f47be2c7887610b
thomasccp <thomasccp@msn.com>
2013-11-20 23:39:30 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :correct_user,   only: [:edit, :update]
+	before_action :admin_user,     only: :destroy
+	before_action :signed_in_user_filter, only: [:new, :create]
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
 
 	def show
 		@user = User.find(params[:id])
@@ -19,8 +27,50 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User deleted."
+		redirect_to users_url
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	# Before filters
+	
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_url, notice: "Please sign in."
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_url) unless current_user.admin?
+	end
+
+	def signed_in_user_filter
+		if signed_in?
+			redirect_to root_url, notice: "Already logged in."
+		end
 	end
 end
